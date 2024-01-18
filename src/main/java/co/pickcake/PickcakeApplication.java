3842bf08087fca8fd90adc3d91c080a81d15cbcb
Hail-cali <willnotproud5@gmail.com>
2024-01-18 22:44:47 +0900

    Initial commit

---
+++ src/main/java/co/pickcake/PickcakeApplication.java
@@ -0,0 +1,13 @@
+package co.pickcake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickcakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickcakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/pickcake/PickcakeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.pickcake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickcakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
