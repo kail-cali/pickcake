3e75bb7ee2db1a151e8532e735cb31bbe32aacca
Hail-cali <willnotproud5@gmail.com>
2024-02-05 19:01:26 +0900

    add: 프론트 반영 관련 messages 기능 추가 =========================================================== - 공동 작업을 위해 mock 선 반영 - messages 프로퍼티 추가    - 추후 상세 파라미터 추가 예정 - 메시지 국제화 추가    - 프론트 반영본에 따라 변경 예정

---
+++ src/test/java/co/pickcake/messages/MessagesSourceTest.java
@@ -0,0 +1,34 @@
+package co.pickcake.messages;
+
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.stereotype.Repository;
+
+import java.util.Locale;
+
+@SpringBootTest
+
+public class MessagesSourceTest {
+
+    @Autowired
+    private  MessageSource messageSource;
+
+    @Test
+    public void helloMessages() {
+        String hello = messageSource.getMessage("test.hello", null, Locale.ENGLISH);
+        Assertions.assertThat(hello).isEqualTo("hello");
+
+        String hello2 = messageSource.getMessage("test.hello", null, Locale.KOREAN);
+        Assertions.assertThat(hello2).isEqualTo("안녕");
+    }
+    @Test
+    public void notFoundMessages(){
+        Assertions.assertThatThrownBy(() -> messageSource.getMessage("noMessage", null, Locale.ENGLISH))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+}
