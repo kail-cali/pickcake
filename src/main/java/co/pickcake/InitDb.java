bc595ff16ea19c4405adce7c7f43c805f622070e
Hail-cali <willnotproud5@gmail.com>
2024-02-08 18:22:29 +0900

    modified: config 관련 파일 업데이트 ## NEED TO FIX THIS COMMIT =========================================================== [임시 업로드] - 공동 작업을 위해 config 정보를 commit - 우선은 dev 관련 config 만 업데이트, deploy config 는 이러한 방식으로 공유하지 않을 예정

--- src/main/java/co/pickcake/InitDb.java
+++ src/main/java/co/pickcake/InitDb.java
@@ -47,10 +47,9 @@ public class InitDb {
     public void init() {
 
         if (service != "test") {
-            //        initService.dbInitWithItem();
             initService.dbInitWithImageItem();
+            initService.dbInitWithMember();
 
-//        initService.dbInitWithOrder();
         }
     }
 
@@ -59,11 +58,7 @@ public class InitDb {
         if (service == "test") {
             if (type.equals(1)) {
                 return initService.dbInitWithImageItem();
-
-            } else if (type.equals(2)) {
-                return initService.dbInitWithOrder();
             }
-
         }
         return 0;
     }
@@ -205,52 +200,57 @@ public class InitDb {
         }
 
 
-        public Integer dbInitWithOrder() {
-            Member member = Member.createMember("userA", "서울", "연세로,", "31122");
+//        public int dbInitWithOrder() {
+//            Member member = Member.createMember("userA", "서울", "연세로,", "31122");
+//
+//            em.persist(member);
+//
+//            Cake cake1 = Cake.createCakeWithImage("생크림 케이크", "신라호텔", "우유 생크림이 들어간 케이크",
+//                    150000, fileUuidGeneratePolicy);
+//
+//            em.persist(cake1);
+//
+//            CakeCategory cakeCategory1 = CakeCategory.createCategory("생일");
+//            em.persist(cakeCategory1);
+//
+//
+//            EventCakeCategory eventCakeCategory1 = EventCakeCategory.addCakeCategory(cake1,cakeCategory1);
+//
+//            em.persist(eventCakeCategory1);
+//
+//            Cake cake2 = Cake.createCakeWithImage("딸기 가득 시루떡 케이크", "성심당", "초코 카스테라에 딸기가 가득 올라간 케이크",
+//                    210000, fileUuidGeneratePolicy);
+//            em.persist(cake2);
+//
+//            CakeCategory cakeCategory2 = CakeCategory.createCategory("12월 크리스마스");
+//            em.persist(cakeCategory2);
+//
+//            EventCakeCategory eventCakeCategory2 = EventCakeCategory.addCakeCategory(cake2,cakeCategory2);
+//            em.persist(eventCakeCategory2);
+//
+//            EventCakeCategory eventCakeCategory3 = EventCakeCategory.addCakeCategory(cake2,cakeCategory1);
+//            em.persist(eventCakeCategory3);
+//
+//
+//            /* order items */
+//            OrderItem orderItem1 = OrderItem.createOrderItem(cake1, 1300000, 0);
+//            OrderItem orderItem2 = OrderItem.createOrderItem(cake2, 210000, 0);
+//
+//            Delivery delivery = new Delivery();
+//            delivery.setAddress(member.getAddress());
+//
+//            Order order = Order.createOrder(member, delivery, orderItem1);
+//            em.persist(order);
+//            return 4;
+//        }
+
+
+        public void dbInitWithMember() {
+            Member member = Member.createMember("hail1", "cali", "hhh111" ,"서울", "연세로", "31122");
 
             em.persist(member);
 
-            Cake cake1 = Cake.createCakeWithImage("생크림 케이크", "신라호텔", "우유 생크림이 들어간 케이크",
-                    150000, fileUuidGeneratePolicy);
-
-            em.persist(cake1);
-
-            CakeCategory cakeCategory1 = CakeCategory.createCategory("생일");
-            em.persist(cakeCategory1);
-
-
-            EventCakeCategory eventCakeCategory1 = EventCakeCategory.addCakeCategory(cake1,cakeCategory1);
-
-            em.persist(eventCakeCategory1);
-
-            Cake cake2 = Cake.createCakeWithImage("딸기 가득 시루떡 케이크", "성심당", "초코 카스테라에 딸기가 가득 올라간 케이크",
-                    210000, fileUuidGeneratePolicy);
-            em.persist(cake2);
-
-            CakeCategory cakeCategory2 = CakeCategory.createCategory("12월 크리스마스");
-            em.persist(cakeCategory2);
-
-            EventCakeCategory eventCakeCategory2 = EventCakeCategory.addCakeCategory(cake2,cakeCategory2);
-            em.persist(eventCakeCategory2);
-
-            EventCakeCategory eventCakeCategory3 = EventCakeCategory.addCakeCategory(cake2,cakeCategory1);
-            em.persist(eventCakeCategory3);
-
-
-            /* order items */
-            OrderItem orderItem1 = OrderItem.createOrderItem(cake1, 1300000, 0);
-            OrderItem orderItem2 = OrderItem.createOrderItem(cake2, 210000, 0);
-
-            Delivery delivery = new Delivery();
-            delivery.setAddress(member.getAddress());
-
-            Order order = Order.createOrder(member, delivery, orderItem1);
-            em.persist(order);
-            return 4;
         }
-
-
-
 
     }
 }
